19daefbfed0bcb8568179b242fdf8b222332db93
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-07-22 17:44:12 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        if(target.length != arr.length)
+            return false;
+        else {
+            Arrays.sort(target);
+            Arrays.sort(arr);
+
+            for(int i = 0; i< target.length; i++){
+                if(target[i] != arr[i])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
